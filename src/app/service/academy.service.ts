75ef654f74d8e808f9d415d9f9fe2e9894b920c7
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-12-05 10:17:48 +0100

    ..

--- src/app/service/academy.service.ts
+++ src/app/service/academy.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Academy } from '../model/academy.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class AcademyService {
   constructor(private http: HttpClient) { }
 
   getAllAcademies(){
-    return this.http.get<Academy[]>("/api/sport/all");
+    return this.http.get<Academy[]>("api/academy/all");
   }
 
 }
