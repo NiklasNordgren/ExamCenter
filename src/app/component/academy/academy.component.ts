0111679cbf689b7561c7826bd7d070dfb81c665a
SannaLundqvist <lundqvist174@gmail.com>
2019-12-09 10:54:56 +0100

    konstig push

--- src/app/component/academy/academy.component.ts
+++ src/app/component/academy/academy.component.ts
@@ -1,35 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { AcademyService } from 'src/app/service/academy.service';
+import { SubjectService } from 'src/app/service/subject.service';
 
 @Component({
   selector: 'app-academy',
   templateUrl: './academy.component.html',
   styleUrls: ['./academy.component.scss']
 })
-export class AcademyComponent implements OnInit {
+export class AcademyComponent implements OnInit, OnDestroy {
   subscriptions = new Subscription();
   private shortHeader = 'Abbrivation';
-  private name = 'Academy';
-  private data = [{
-    name: 'Matematik',
-    shortDesc: 'MA'
-  },{
-    name: 'Datakunskap',
-    shortDesc: 'DA'
-  }];
+  private name = 'Subject';
+  private data = [];
 
-  constructor(private route: ActivatedRoute, private service: AcademyService) {}
+  constructor(private route: ActivatedRoute, private service: SubjectService) { }
 
   ngOnInit() {
     this.subscriptions.add(this.route.paramMap.subscribe(params => {
       let academyId = parseInt(params.get("id"));
-      this.service.getAllAcademies().subscribe(academy => {
-        console.log('logging academy from db..: ' + academy);
-        
-      });
+      this.setSubjetsByAcademyId(academyId);
+
     }));
   }
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 
+  setSubjetsByAcademyId(academyId) {
+    this.service.getAllSubjectsByAcademyId(academyId).subscribe(subjects => {
+      this.data = [];
+      subjects.forEach(subject => {
+        this.data.push({
+          name: subject['name'],
+          shortDesc: subject['code']
+        });
+      });
+    });
+
+  }
 }
