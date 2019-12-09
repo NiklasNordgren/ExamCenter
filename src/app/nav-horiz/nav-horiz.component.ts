c6b4876aa0764d15508501410967e943ac238267
hannameden <hannameden@gmail.com>
2019-12-09 09:14:28 +0100

    navbar

--- src/app/nav-horiz/nav-horiz.component.ts
+++ src/app/nav-horiz/nav-horiz.component.ts
@@ -1,6 +1,27 @@
 
 
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
+@Component({
+  selector: 'app-nav-horiz',
+  templateUrl: './nav-horiz.component.html',
+  styleUrls: ['./nav-horiz.component.scss']
+})
+export class NavHorizComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
+/*
 
 import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button'; 
@@ -20,4 +41,4 @@ export class NavHorizComponent implements OnInit {
   ngOnInit() {
   }
 
-}
+}*/
