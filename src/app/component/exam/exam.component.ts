4dd27b2678c3e67bd7850ad13b400609c13da904
Thomas Lundgren <thomasLundgren@live.com>
2020-01-02 09:08:24 +0100

    Change CourseComponent bug

--- src/app/component/exam/exam.component.ts
+++ src/app/component/exam/exam.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import { ExamService } from 'src/app/service/exam.service';
-import { FileService } from 'src/app/file.service';
-import { Navigator } from 'src/app/util/navigator';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
+import { ExamService } from "src/app/service/exam.service";
+import { FileService } from "src/app/file.service";
+import { Navigator } from "src/app/util/navigator";
 
 @Component({
-	selector: 'app-exam',
-	templateUrl: './exam.component.html',
-	styleUrls: ['./exam.component.scss'],
+	selector: "app-exam",
+	templateUrl: "./exam.component.html",
+	styleUrls: ["./exam.component.scss"],
 	providers: [Navigator]
 })
 export class ExamComponent implements OnInit, OnDestroy {
 	private subscriptions = new Subscription();
-	name = 'Filename';
+	name = "Filename";
 	data = [];
 
 	constructor(
@@ -26,7 +26,7 @@ export class ExamComponent implements OnInit, OnDestroy {
 	ngOnInit() {
 		this.subscriptions.add(
 			this.route.paramMap.subscribe(params => {
-				const courseId = parseInt(params.get('id'), 10);
+				const courseId = parseInt(params.get("id"), 10);
 				this.setExamsByCourseId(courseId);
 			})
 		);
@@ -37,28 +37,29 @@ export class ExamComponent implements OnInit, OnDestroy {
 	}
 
 	setExamsByCourseId(courseId) {
-		const sub = this.service.getAllExamsByCourseId(courseId).subscribe(exams => {
-			this.data = [];
-			exams.forEach(exam => {
-				console.log(exam.fileName);
+		const sub = this.service
+			.getAllExamsByCourseId(courseId)
+			.subscribe(exams => {
+				this.data = [];
+				exams.forEach(exam => {
+					console.log(exam.fileName);
 
-				this.data.push({
-					id: exam.fileName,
-					name: exam.fileName,
-					shortDesc: ''
+					this.data.push({
+						id: exam.fileName,
+						name: exam.fileName,
+						shortDesc: ""
+					});
 				});
 			});
-		});
 		this.subscriptions.add(sub);
 	}
 
 	openPdf(row) {
-		console.log('Row: ' + row);
-		console.log('Row class: ' + row.class);
+		console.log("Row: " + JSON.stringify(row));
 		const fileName = row.id;
 		const sub = this.fileService.downloadFile(fileName).subscribe(pdfBlob => {
 			const fileURL = URL.createObjectURL(pdfBlob);
-			window.open(fileURL, '_blank');
+			window.open(fileURL, "_blank");
 		});
 		this.subscriptions.add(sub);
 	}
