6e09ce959214194803d78eb18d7e85de78939f8b
Thomas Lundgren <thomasLundgren@live.com>
2020-01-03 11:47:02 +0100

    Remove unnecessary OAuth imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-// import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
-// import { authConfig } from './sso.config';
-import { Observable } from 'rxjs';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { map, shareReplay } from 'rxjs/operators';
-import { AcademyService } from 'src/app/service/academy.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Breakpoints, BreakpointObserver } from "@angular/cdk/layout";
+import { map, shareReplay } from "rxjs/operators";
+import { AcademyService } from "src/app/service/academy.service";
+import { Router } from "@angular/router";
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-
-	constructor(private breakpointObserver: BreakpointObserver, private service: AcademyService, private router: Router) {
-
-	}
+	constructor(
+		private breakpointObserver: BreakpointObserver,
+		private service: AcademyService,
+		private router: Router
+	) {}
 
 	private academies = [];
-	isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+	isHandset$: Observable<boolean> = this.breakpointObserver
+		.observe(Breakpoints.Handset)
 		.pipe(
 			map(result => result.matches),
 			shareReplay()
@@ -28,7 +28,6 @@ export class AppComponent implements OnInit {
 	ngOnInit() {
 		this.service.getAllAcademies().subscribe(responseAcademies => {
 			this.convertAndSetAcademies(responseAcademies);
-
 		});
 	}
 	convertAndSetAcademies(responseAcademies) {
@@ -45,6 +44,6 @@ export class AppComponent implements OnInit {
 		this.router.navigate([`${pageName}`]);
 	}
 	goToHomePage() {
-		this.goToPage('/');
+		this.goToPage("/");
 	}
 }
