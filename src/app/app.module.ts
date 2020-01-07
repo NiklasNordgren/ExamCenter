b261b9e6d8410d35a3501a54dbf3f4c4f4d3a4e9
SannaLundqvist <lundqvist174@gmail.com>
2020-01-07 11:01:36 +0100

    removed dupe dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,81 +5,6 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatSliderModule } from '@angular/material/slider';
-import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { AddressFormComponent } from './address-form/address-form.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
-import { TableComponent } from './component/table/table.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
-import { TreeComponent } from './tree/tree.component';
-import { MatTreeModule } from '@angular/material/tree';
-import { DragDropComponent } from './drag-drop/drag-drop.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatProgressBarModule } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { OAuthModule } from 'angular-oauth2-oidc';
-import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
-import { LogoutComponent } from './logout/logout.component';
-import { FileUploadComponent } from './component/file-upload/file-upload.component';
-import { SelectExamPropertiesComponent } from './component/select-exam-properties/select-exam-properties.component';
-
-import { MatSliderModule } from "@angular/material/slider";
-import { NavComponent } from "./nav/nav.component";
-import { LayoutModule } from "@angular/cdk/layout";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
-import { AddressFormComponent } from "./address-form/address-form.component";
-import { MatInputModule } from "@angular/material/input";
-import { MatSelectModule } from "@angular/material/select";
-import { MatRadioModule } from "@angular/material/radio";
-import { MatCardModule } from "@angular/material/card";
-import { ReactiveFormsModule } from "@angular/forms";
-import { TableComponent } from "./component/table/table.component";
-import { MatTableModule } from "@angular/material/table";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatSortModule } from "@angular/material/sort";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatMenuModule } from "@angular/material/menu";
-import { TreeComponent } from "./tree/tree.component";
-import { MatTreeModule } from "@angular/material/tree";
-import { DragDropComponent } from "./drag-drop/drag-drop.component";
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { MatProgressBarModule, MatCheckboxModule } from "@angular/material";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule } from "@angular/forms";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-
-import { HttpClientModule } from "@angular/common/http";
-import { HomeComponent } from "./home/home.component";
-import { LogoutComponent } from "./logout/logout.component";
-import { FileUploadComponent } from "./component/file-upload/file-upload.component";
-import { SelectExamPropertiesComponent } from "./component/select-exam-properties/select-exam-properties.component";
-
 import { FileUploadModule } from "ng2-file-upload";
 import { OutboxComponent } from "./component/outbox/outbox.component";
 import { ConfirmationDialog } from "./component/confirmation-dialog/confirmation-dialog";
@@ -115,16 +40,6 @@ import { SubjectFormComponent } from './component/subject-form/subject-form.comp
 @NgModule({
 	declarations: [
 		AppComponent,
-		NavComponent,
-		AddressFormComponent,
-		TableComponent,
-		DashboardComponent,
-		TreeComponent,
-		DragDropComponent,
-		LoginComponent,
-		HomeComponent,
-		LogoutComponent,
-		FileUploadComponent,
 		NavHorizComponent,
 		ListComponent,
 		AcademyComponent,
@@ -139,16 +54,6 @@ import { SubjectFormComponent } from './component/subject-form/subject-form.comp
 		ExamHandlerComponent,
 		AcademyFormComponent,
 		AppComponent,
-		NavComponent,
-		AddressFormComponent,
-		TableComponent,
-		DashboardComponent,
-		TreeComponent,
-		DragDropComponent,
-		LoginComponent,
-		HomeComponent,
-		LogoutComponent,
-		FileUploadComponent,
 		NavHorizComponent,
 		ListComponent,
 		AcademyComponent,
@@ -157,7 +62,6 @@ import { SubjectFormComponent } from './component/subject-form/subject-form.comp
 		CourseComponent,
 		ExamComponent,
 		AboutComponent,
-		SelectExamPropertiesComponent,
 		OutboxComponent,
 		ConfirmationDialog,
 		TestSwipeComponent,
@@ -169,68 +73,18 @@ import { SubjectFormComponent } from './component/subject-form/subject-form.comp
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		HttpClientModule,
 		BrowserAnimationsModule,
-		MatSliderModule,
-		LayoutModule,
-		MatToolbarModule,
-		MatButtonModule,
-		MatSidenavModule,
-		MatIconModule,
-		MatListModule,
-		MatInputModule,
-		MatProgressSpinnerModule,
-		MatSelectModule,
-		MatRadioModule,
-		MatCardModule,
-		ReactiveFormsModule,
-		MatTableModule,
-		MatPaginatorModule,
-		MatSortModule,
-		MatGridListModule,
-		MatMenuModule,
-		MatTreeModule,
-		MatProgressBarModule,
-		DragDropModule,
 		// OAuthModule.forRoot()\r\nNgxFileDropModule,
 		FileUploadModule,
 		FontAwesomeModule,
-		FlexLayoutModule,
-		FormsModule,
 		FontAwesomeModule,
-		MatCheckboxModule,
 		BrowserModule,
 		AppRoutingModule,
-		HttpClientModule,
 		BrowserAnimationsModule,
-		MatSliderModule,
-		LayoutModule,
-		MatToolbarModule,
-		MatButtonModule,
-		MatSidenavModule,
-		MatIconModule,
-		MatListModule,
-		MatInputModule,
-		MatProgressSpinnerModule,
-		MatSelectModule,
-		MatRadioModule,
-		MatCardModule,
-		ReactiveFormsModule,
-		MatTableModule,
-		MatPaginatorModule,
-		MatSortModule,
-		MatGridListModule,
-		MatMenuModule,
-		MatTreeModule,
-		MatProgressBarModule,
-		DragDropModule,
 		// OAuthModule.forRoot()\r\nNgxFileDropModule,
 		FileUploadModule,
 		FontAwesomeModule,
-		FlexLayoutModule,
-		FormsModule,
 		FontAwesomeModule,
-		MatDatepickerModule,
 		MatDialogModule,
 		MatTabsModule
 	],
