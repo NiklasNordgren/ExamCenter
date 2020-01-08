584a2bb5c5d2ca85d8dce50aa60e4ada5338848b
SannaLundqvist <lundqvist174@gmail.com>
2020-01-08 15:55:27 +0100

    course-handler är klar
    unpublish är gjord,
    kvar är course-component

--- src/app/service/course.service.ts
+++ src/app/service/course.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Course } from '../model/course.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,15 @@ export class CourseService {
 
   constructor(private http: HttpClient) { }
 
-  getAllCoursesBySubjectId(subjectId: number){
-    return this.http.get<Course[]>("api/courses/subject/" + subjectId);
+  getAllCoursesBySubjectId(subjectId: number) {
+    return this.http.get<Course[]>("api/courses/published/subject/" + subjectId);
   }
 
-  getAllCourses(){
+  getAllCourses() {
     return this.http.get<Course[]>("api/courses/all");
+  }
+
+  unpublishCourses(courses: Course[]): Observable<Course> {
+    return this.http.post<Course>('/api/courses/unpublish/' + true, courses);
   }
 }
