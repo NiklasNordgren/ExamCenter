adb6f1385e8ebbb8fc67278fe7b736d8b97cf18a
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-01-08 12:04:45 +0100

    Added guards for admin & superuser

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
 import { User } from '../model/user.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
 
-	constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-	getAllUsers() {
-		return this.http.get<User[]>("api/users/all");
+    getAllUsers() {
+        return this.http.get<User[]>("api/users/all");
     }
 
     getUserById(id: number) {
         return this.http.get<User>('api/users/' + id);
     }
-  
+
     saveUser(user: User): Observable<User> {
         return this.http.post<User>("/api/users", user);
     }
@@ -25,6 +26,14 @@ export class UserService {
     deleteUser(id: number) {
         return this.http.delete('/api/users/' + id).subscribe(data => {
         });;
+    }
+
+    isUserLoggedInAsAdmin(): Observable<boolean> {
+        return this.http.get<boolean>('api/users/is/admin');
+    }
+
+    isUserLoggedInAsSuperUser() {
+        return this.http.get('api/users/is/superuser');
     }
 
 }
