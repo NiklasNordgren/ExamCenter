19983061908d8971515d6a9ee472024a927743ac
Thomas Lundgren <thomasLundgren@live.com>
2020-01-16 11:46:35 +0100

    Remove information icon from searchcomponent

--- src/app/service/settings.service.ts
+++ src/app/service/settings.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Settings } from './../model/settings.model';
-import { Observable } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
