06db0407d3b3479c93813e2d1452b8d7f5bb5418
Thomas Lundgren <thomasLundgren@live.com>
2020-01-17 13:30:07 +0100

    Install observable-profiler

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Observable } from "rxjs";
+import { setup, track, printSubscribers } from "observable-profiler";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import 'hammerjs';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+import "hammerjs";
 
 if (environment.production) {
 	enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+	.bootstrapModule(AppModule)
+	.catch(err => console.error(err));
+
+setup(Observable);
+platformBrowserDynamic([])
+	.bootstrapModule(AppModule)
+	.then(ref => {
+		track();
+		(window as any).stopProfiler = () => {
+			ref.destroy();
+			const subscribers = track(false);
+			printSubscribers({
+				subscribers
+			});
+		};
+	})
 	.catch(err => console.error(err));
