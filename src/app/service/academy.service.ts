68de39c5dceb53a775e27afd2746944d109f6cde
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-01-29 20:58:28 +0100

    update

--- src/app/service/academy.service.ts
+++ src/app/service/academy.service.ts
@@ -8,12 +8,12 @@ import { Observable } from 'rxjs';
 })
 export class AcademyService {
 
-	private academyUrl = 'backend:9000/academies/';
+	//private academyUrl = 'backend:9000/academies/';
 
 	constructor(private http: HttpClient) { }
 
 	getAllAcademies() {
-		return this.http.get<Academy[]>(this.academyUrl + 'all');
+		return this.http.get<Academy[]>('/api/academies/all');
 	}
 
 	getAcademyById(id: number) {
